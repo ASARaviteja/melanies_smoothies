b33b87677d2092c6db4bcc7b79a253f379829deb
ASARaviteja <133795928+ASARaviteja@users.noreply.github.com>
2026-09-16 22:37:23 +0530

    Update streamlit_app.py
    Indent fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,11 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string+ """','""" + name_on_order + """')"""
     
     time_to_insert = st.button('Submit Order')
-
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,' + name_on_order + '!', icon="✅")
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
